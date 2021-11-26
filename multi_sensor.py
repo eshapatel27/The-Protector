cfd283933716c141f881b267568fa0c9f6ecadd3
lilyxburke <94839155+lilyxburke@users.noreply.github.com>
2021-11-26 08:17:12 +0000

    Update multi_sensor.py

--- multi_sensor.py
+++ multi_sensor.py
@@ -1,1 +1,38 @@
+import RPi.GPIO as gpio
+import time
 
+def distance(measure-"cm"):
+  try:
+    gpio.setmode(gpio.BOARD)
+    gpio.setup(12, gpio.OUT)
+    gpio.setup(16, gpio.IN)
+    #input the pin location we're using#
+    
+    gpio.output(12, False)
+    while gpio.input(16) == 0:
+      nosig = time.time()
+    
+    while gpio.input(16) == 1:
+      sig = time.time()
+      
+    tl = sig - nosig
+    
+    if measure == "cm":
+      distance = tl / 0.000058
+    elif measure == "in":
+      distance = tl / 0.000148
+    else:
+      print("Improper choice of measurement: in or cm")
+      distance = None
+    
+    gpio.cleanup()
+    return distance
+  
+  except:
+    distance = 100
+    gpio.cleanup()
+    return distance
+  
+  if __name__ == "__main__":
+    print(distance, "cm")
+  
