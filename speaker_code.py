af29c00e238988426e4b34cfbc27bcfd9cb54670
lilyxburke <94839155+lilyxburke@users.noreply.github.com>
2021-11-27 14:48:30 +0000

    Update speaker_code.py

--- speaker_code.py
+++ speaker_code.py
@@ -10,3 +10,5 @@ elif distance <= 300 and distance > 200:
     winsound.PlaySound("DRUMROLL.wav", winsound.SND_FILENAME)
 elif distance <= 200 and distance > 100:
     winsound.PlaySound("HEARTBEAT.wav", winsound.SND_FILENAME)
+    
+##should be able to connect speaker using bluetooth##
