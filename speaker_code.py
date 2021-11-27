2bef58f469dbb1d72a57090745f8f5016e14e3e5
lilyxburke <94839155+lilyxburke@users.noreply.github.com>
2021-11-27 14:55:23 +0000

    Update speaker_code.py

--- speaker_code.py
+++ speaker_code.py
@@ -1,7 +1,7 @@
 import winsound
 import random
 
-distance = random.randint(101,400)
+distance = random.randint(51,400)
 print(distance)
 
 if distance <= 400 and distance > 300:
@@ -10,5 +10,7 @@ elif distance <= 300 and distance > 200:
     winsound.PlaySound("DRUMROLL.wav", winsound.SND_FILENAME)
 elif distance <= 200 and distance > 100:
     winsound.PlaySound("HEARTBEAT.wav", winsound.SND_FILENAME)
+elif distance <= 100 and > 50:
+    wninsound.PlaySound("10kHZ.wav.wav", winsound.SND_FILENAME)
     
 ##should be able to connect speaker using bluetooth##
